a8c12f5089a6c9b4bf92269c7c673de96945347b
PamelaCleveland <pamelac021@gmail.com>
2021-11-28 05:45:30 -0500

    thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,28 +1,42 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
-const ReactionSchema = new Schema(
-    {
-
-
-
-    })
+const ReactionSchema = new Schema({});
 
 const ThoughtSchema = new Schema(
-    {
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    // timestamp
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
 
+    username: {
+      type: String,
+      required: true,
+    },
 
-    })
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
 
+    id: false,
+  }
+);
 
-
-
-
-
-
-ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
-          
-const Thought = model('Thought', ThoughtSchema);
-          
+
+const Thought = model("Thought", ThoughtSchema);
+
 module.exports = Thought;
