fa00430df9c7b458cd6bd7a937a8b6e4fbc2e798
PamelaCleveland <pamelac021@gmail.com>
2021-11-28 05:38:24 -0500

    user model

--- models/User.js
+++ models/User.js
@@ -1,19 +1,52 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
-    {
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+        "Valid email address required",
+      ],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 
-    })
-
-
-
-
-
-
-
-
-
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
