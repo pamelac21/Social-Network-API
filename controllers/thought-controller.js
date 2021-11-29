1f95bc4ed128fe439996602df845b3d4b363a8ce
PamelaCleveland <pamelac021@gmail.com>
2021-11-28 21:49:04 -0500

    reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -86,6 +86,36 @@ const thoughtController = {
       })
       .catch((err) => res.json(err));
   },
+  // reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id!" });
+          return;
+        }
+
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+  // delete reaction
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = thoughtController;
